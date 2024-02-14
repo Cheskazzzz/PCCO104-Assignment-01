60c8f5b1e618a7c36f9d2e439f28098acc91e078
Cheskazzzz <159403221+Cheskazzzz@users.noreply.github.com>
2024-02-14 12:44:03 +0000

    Lab Activity

--- main.cs
+++ main.cs
@@ -2,63 +2,71 @@ using System;
 
 class Program
 {
-    static void Main()
-    {
-        Console.WriteLine("Enter the denomination of the Philippine Bank Note (Bills and Coins): ");
-        double denomination;
+  static void Main()
+  {
+    Console.WriteLine("Enter the denomination of the Philippine Bank Note (Bills and Coins): ");
+    double denomination;
 
-        if (double.TryParse(Console.ReadLine(), out denomination))
-        {
-            string personality = GetPersonality(denomination);
-            Console.WriteLine($"Personality found in {denomination}: {personality}");
-        }
-        else
-        {
-            Console.WriteLine($"Invalid Denomination: {Console.ReadLine()}");
-        }
+    if (double.TryParse(Console.ReadLine(), out denomination))
+  {
+    string personality = GetPersonality(denomination);
+    Console.WriteLine($"Personality found in {denomination}: {personality}");
+  }
+  else
+  {
+    Console.WriteLine($"Invaling Denomination:{Console.ReadLine()}");
     }
+  }
 
-    static string GetPersonality(double denomination)
+  static personality GetPersonality(double denomination)
+  {
+    string personality = "No Person is found";
+
+    if (denomination == 1)
     {
-        string personality = "No Person is found";
-
-        if (denomination == 1)
-        {
-            personality = "Jose Rizal";
-        }
-        else if (denomination == 5)
-        {
-            personality = "Emilio Aguinaldo";
-        }
-        else if (denomination == 10)
-        {
-            personality = "Andres Bonifacio, Apolinario Mabini";
-        }
-        else if (denomination == 20)
-        {
-            personality = "Manuel L. Quezon";
-        }
-        else if (denomination == 50)
-        {
-            personality = "Sergio Osmena";
-        }
-        else if (denomination == 100)
-        {
-            personality = "Manuel Roxas";
-        }
-        else if (denomination == 200)
-        {
-            personality = "Diosdado Macapagal";
-        }
-        else if (denomination == 500)
-        {
-            personality = "Benigno Sr. and Corazon Aquino";
-        }
-        else if (denomination == 1000)
-        {
-            personality = "Jose Abad Santos, Vicente Lim, Josefa Llanes Escoda";
-        }
-
-        return personality;
+      personality = "Jose Rizal";
     }
-}
+    else if (denomination == 5)
+    {
+      personality = "Emilio Aguinaldo";
+      }
+    else if (denomination == 10)
+    {
+      personality ="Andres Bonifacio, Apolinario Mabini";
+      }
+    else if (denomination == 20)
+    {
+      personality ="Manuel L. Quezon";
+      }
+    else if (denomination == 50)
+    {
+      personality ="Sergio Osmena";
+      }
+    else if (denomination == 100)
+    {
+      personality ="Manuel Roxas";
+      }
+    else if (denomination == 200)
+    {
+      personality ="Diosdado Macapagal";
+      }
+    else if (denomination == 500)
+    {
+      personality ="Benigno Sr. and Corazon Aquino";
+      }
+    else if (denomination == 1000)
+    {
+      personality ="Jose Abad Santos, Vincent Lim, Josefa Llanes Escoda";
+      }
+
+    return personality
+    }
+  }
+
+
+    
+  
+
+  
+  
+    
